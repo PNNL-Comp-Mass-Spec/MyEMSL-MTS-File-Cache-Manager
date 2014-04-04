684efda63955f2da775fc7b4d8afcf0fcbe80fd4
Matthew Monroe <matthew.monroe@pnnl.gov>
2014-04-04 04:59:20 +0000

    Add batch file

---
+++ bin/Distribute_Program.bat
@@ -0,0 +1,5 @@
+
+xcopy debug\MyEMSL_MTS_File_Cache_Manager.exe \\proteinseqs\dms_programs\MyEMSL_Cache_Manager\ /y /d
+xcopy debug\*.dll \\proteinseqs\dms_programs\MyEMSL_Cache_Manager\ /y /d
+
+pause
