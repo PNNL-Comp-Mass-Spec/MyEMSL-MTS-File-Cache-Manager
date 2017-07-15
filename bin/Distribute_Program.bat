59f11b35b020060ce835b332cedfca889b3e6f72
alchemistmatt <matthew.monroe@pnnl.gov>
2017-07-14 18:15:35 -0700

    Update the batch file

--- bin/Distribute_Program.bat
+++ bin/Distribute_Program.bat
@@ -1,5 +1,9 @@
+rem Primary Location
+xcopy debug\MyEMSL_MTS_File_Cache_Manager.exe \\ProteinSeqs\dms_programs\MyEMSL_Cache_Manager\ /y /d
+xcopy debug\*.dll \\ProteinSeqs\dms_programs\MyEMSL_Cache_Manager\ /y /d
 
-xcopy debug\MyEMSL_MTS_File_Cache_Manager.exe \\proteinseqs\dms_programs\MyEMSL_Cache_Manager\ /y /d
-xcopy debug\*.dll \\proteinseqs\dms_programs\MyEMSL_Cache_Manager\ /y /d
+rem Backup copy
+xcopy debug\MyEMSL_MTS_File_Cache_Manager.exe \\Sylvester\dms_programs\MyEMSL_Cache_Manager\ /y /d
+xcopy debug\*.dll \\Sylvester\dms_programs\MyEMSL_Cache_Manager\ /y /d
 
 pause
